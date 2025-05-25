338bd302504bd92d29cc981f88ee0f1556a32d20
Millennium Roy <124084501+millenniumroy11@users.noreply.github.com>
2025-05-25 14:04:28 +0530

    Delete CrowdFunding directory

--- CrowdFunding/donations/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Donation
-
-@admin.register(Donation)
-class DonationAdmin(admin.ModelAdmin):
-    list_display = ('donor', 'project', 'amount', 'is_anonymous', 'created_at')
-    list_filter = ('is_anonymous', 'created_at')
-    search_fields = ('donor__username', 'project__title', 'message')
-    readonly_fields = ('created_at',)
